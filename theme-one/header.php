50511eb11fe2adc01a0b155c5906adb641a35ea8
Mobaraque7 <mk2956806@gmail.com>
2023-05-28 11:06:28 +0600

    change is style

--- theme-one/header.php
+++ theme-one/header.php
@@ -35,7 +35,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-3">
-        <a href=""><img src="<?php echo get_theme_mod('hader_logo'); ?>" alt=""></a>
+        <a href=""><img src="<?php echo get_theme_mod('hader_logo'); ?>"; class ="img-log" alt=""></a>
         </div>
         <div class="col-md-9">
         <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
